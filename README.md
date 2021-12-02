33d46b46e7b986e5dc56044768e648345590bb6e
Kaio Henrik <kaio.henrik@sga.pucminas.br>
2021-12-02 17:35:40 -0300

    treinando a funcionalidade teste dentro do visual studio

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 ## Desafios e Testes
 * [Exercício: Git/GitHub e Markdown](https://github.com/kaiohenrikk/bootcamp-dio/tree/main/exercicio-git-github-e-markdown)
 * [Desafio 1 - Criação e Organização do Repositório Git/Github](https://github.com/kaiohenrikk/bootcamp-dio/tree/main/desafio-criacao-organizacao-do-repositorio)
-* [Exercício: Instalação do Visual Studio e Gerando Código e Debugging](https://github.com/kaiohenrikk/bootcamp-dio/tree/main/exercicio-ide-visual-code/primeiro-programa-c%23)
+* [Exercício: Instalação do Visual Studio, Gerando Código e Debugging, Teste](https://github.com/kaiohenrikk/bootcamp-dio/tree/main/exercicio-ide-visual-code/primeiro-programa-c%23)
---
+++ exercicio-ide-visual-code/primeiro-programa-c#/PrimeiroTests/ContasTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Contas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contas.Tests
+{
+    [TestClass()]
+    public class ContasTests
+    {
+        [TestMethod()]
+        public void somaTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 10;
+            var contas = new Contas();
+
+            var result = contas.soma(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void subtracaoTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 0;
+            var contas = new Contas();
+
+            var result = contas.subtracao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void divisaoTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 1;
+            var contas = new Contas();
+
+            var result = contas.divisao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void multiplicacaoTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 25;
+            var contas = new Contas();
+
+            var result = contas.multiplicacao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+    }
+}
